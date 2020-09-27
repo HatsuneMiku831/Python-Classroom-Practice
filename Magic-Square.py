6a8eb834a44f6e78dbfa5e23030ac28d3b88d318
Miku <hatsunemiku2871@gmail.com>
2020-09-27 19:19:45 +0800

    Create Magic-Square.py

---
+++ Magic-Square.py
@@ -0,0 +1,47 @@
+square = []
+
+n = 3
+
+for i in range(n + 1):
+
+    square.append([0] * (n + 1))
+
+i = 0
+j = (n + 1) // 2
+
+for key in range(1, n ** 2 + 1):
+
+    if key % n == 1:
+
+        i += 1
+
+    else:
+
+        i -= 1
+        j += 1
+
+    if i == 0:
+
+        i = n
+
+    if j > n:
+
+        j = 1
+
+    square[i][j] = key
+
+matrix = []
+
+for i in range(n):
+
+    matrix.append([0] * n)
+
+for k in range(len(matrix)):
+
+    for l in range(len(matrix[0])):
+
+        matrix[k][l] = square[k + 1][l + 1]
+
+#return matrix
+
+print(matrix)
