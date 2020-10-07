f3d21a6671db94b02c552132466cf791c7981136
Miku <hatsunemiku2871@gmail.com>
2020-10-07 21:11:44 +0800

    Create MS.py

---
+++ MS.py
@@ -0,0 +1,18 @@
+row=0
+a=[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+col=int(len(a)/2)
+a[row][col]=1
+for i in range(2,len(a)**2+1):
+    row_next=(row-1)%5
+    col_next=(col+1)%5
+    if a[row_next][col_next]==0:
+        row_next=(row-1)%5
+        col_next=(col+1)%5
+        a[row_next][col_next]=i
+        row=row_next
+        col=col_next
+    else:
+        a[row+1][col]=i
+        row=row+1
+for s in range(len(a)):
+    print(a[s])
