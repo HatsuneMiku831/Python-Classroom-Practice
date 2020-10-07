7be6740460ad1c48068787777e49c292edb5b45d
Miku <hatsunemiku2871@gmail.com>
2020-10-07 22:49:07 +0800

    Update 202010071.py

--- 202010071.py
+++ 202010071.py
@@ -1,6 +1,9 @@
 num1 = input("請輸入被除數:")
 num2 = input("請輸入除數:")
 
+wx = eval(num1)
+wy = eval(num2)
+
 def GCD(x, y):
 
     r = x % y
@@ -15,3 +18,15 @@ def GCD(x, y):
 
 print("遞迴模式")
 print(GCD(eval(num1), eval(num2)))
+
+wr = wx % wy
+
+while (wr != 0):
+
+    wx = wy
+    wy = wr
+
+    wr = wx % wy
+
+print("迴圈模式")
+print(wy)
